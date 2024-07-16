605ae2b4dcd78a062a411972e62f8d51e3262bdc
Ewans <ew.ewoldt@duocuc.cl>
2024-07-16 05:44:23 -0400

    Cart functions

--- webfinal/cart/cart.py
+++ webfinal/cart/cart.py
@@ -1,4 +1,4 @@
-
+from store.models import Product, Customer
 class Cart():
     def __init__(self, request):
         self.session =request.session
@@ -10,4 +10,52 @@ class Cart():
 
 
         self.cart = cart
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        product_qty = str(quantity)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = int(product_qty)
 
+        self.session.modified = True   
+
+    def cart_total(self):
+        product_ids = self.cart.keys()    
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    total = total + (product.price * value)
+        return total
+    
+    def __len__(self):
+        return len(self.cart)
+
+    def get_products(self):
+        product_ids = self.cart.keys()    
+        products = Product.objects.filter(id__in=product_ids) 
+        return products
+    
+    def get_quantity(self):
+        quantities = self.cart
+        return quantities
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+        current_cart = self.cart
+        current_cart[product_id]=product_qty
+        self.session.modified = True
+
+        updated_cart = self.cart
+        return updated_cart
+    
+    def delete(self, product):
+        product_id = str(product)
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.session.modified = True
--- webfinal/cart/views.py
+++ webfinal/cart/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+from django.contrib import messages
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    cart =Cart(request)
+    cart_products = cart.get_products
+    quantities = cart.get_quantity
+    total= cart.cart_total()
+    context = {"cart_products": cart_products, "quantities": quantities, "total": total}
+    return render(request, "cart_summary.html", context)
 
 def cart_add(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product, quantity=product_qty)
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'quantity': cart_quantity})
+        messages.success(request, '¡Producto añadido al carro de compras!')
+        return response
 
-    pass
+
+    
 def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        cart.delete(product=product_id)
+        messages.success(request, 'Producto eliminado.')
+        response = JsonResponse({'product':product_id})
+    return response
 
-    pass
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        cart.update(product=product_id, quantity= product_qty)
+        messages.success(request, 'Cantidad actualizada.')
+        response = JsonResponse({'qty':product_qty})
+    return response
