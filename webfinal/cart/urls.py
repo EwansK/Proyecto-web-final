6f5a525b061a0df1fe6cff8510fb4acc229fe708
Ewans <ew.ewoldt@duocuc.cl>
2024-07-16 01:32:08 -0400

    Cart views, session_id

--- webfinal/cart/urls.py
+++ webfinal/cart/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home_view
+from .views import cart_summary, cart_add, cart_delete, cart_update
 
 urlpatterns = [
-    path('', home_view, name='home'), 
+    path('cart/cart_summary', cart_summary, name='cart_summary'),
+    path('cart_add/', cart_add, name='cart_add'),
+    path('cart_delete/', cart_delete, name='cart_delete'),
+    path('cart_update/', cart_update, name='cart_update'), 
 
 ] 
